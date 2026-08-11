faf4a3e026451c65fa289d0863b73b85d5b50e5a
Sathvik <76507464+keeeg4n@users.noreply.github.com>
2026-08-11 07:28:03 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 1
+print(type(variable))
+
+variable = 1.0
+print(type(variable))
+
+variable = True
+print(type(variable))
+
+variable = "String"
+print(type(variable))
+
+variable = ["1", "2"]
+print(type(variable))
