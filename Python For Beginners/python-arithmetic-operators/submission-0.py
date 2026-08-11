1dc85b0b8c61f71d0e2f80c5120bdea2c9d4ab68
Sathvik <76507464+keeeg4n@users.noreply.github.com>
2026-08-11 07:33:07 +0530

    Add: python-arithmetic-operators - submission-0

---
+++ Python For Beginners/python-arithmetic-operators/submission-0.py
@@ -0,0 +1,9 @@
+a, b, c = 2, 2, 0.5
+
+sum_result: int = a + b + c
+product_result: int = a * b * c
+
+print(sum_result)
+print(0 - sum_result)
+print(product_result)
+print(sum_result / product_result)
